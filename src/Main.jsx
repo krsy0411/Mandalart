35e9fdc38ac1978e6ffbc9e6cca66e1df4c5f6d9
krsy0411 <krsy0411@naver.com>
2023-09-16 00:39:27 +0900

    한 번 합치기

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,31 +1,140 @@
+import { useState, useEffect } from "react";
 import { Box } from "./components/Box";
 import "./css/main.style.css";
 
-// 서브 페이지 화면
 export const Main = () => {
-  // 중앙 부분들 컬러값
-  const COLORS = ['#FCE1DD', '#FAEDDC', '#FDFCE8', '#EBE9E6','#3A4D8F', '#E9F2DF', '#D8D5EC', '#C7D3EC', '#DCEFFC'];
+  const [mainTopicData, setMainTopicData] = useState({
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: "",
+    six: "",
+    seven: "",
+    eight: "",
+  });
+  const [isEditing, setIsEditing] = useState(null);
+  const [tempData, setTempData] = useState({});
+
+  const handleDataChange = (position, data) => {
+    setMainTopicData((prev) => ({ ...prev, [position]: data }));
+  };
+
+  const handleEdit = (position) => {
+    setIsEditing(position);
+    setTempData((prev) => ({ ...prev, [position]: mainTopicData[position] }));
+  };
+
+  const handleBlur = (position) => {
+    handleDataChange(position, tempData[position]);
+    setIsEditing(null);
+  };
+
+  const positions = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "center",
+    "five",
+    "six",
+    "seven",
+    "eight",
+  ];
+
+  const COLORS = [
+    "#FCE1DD",
+    "#FAEDDC",
+    "#FDFCE8",
+    "#EBE9E6",
+    "#AEDFF7",
+    "#E9F2DF",
+    "#D8D5EC",
+    "#C7D3EC",
+    "#DCEFFC",
+  ];
+
+  useEffect(() => {
+    setMainTopicData({
+      one: "",
+      two: "",
+      three: "",
+      four: "",
+      five: "",
+      six: "",
+      seven: "",
+      eight: "",
+    });
+  }, []);
 
   return (
     <>
       <div className="nav">
         <p className="logo blink">MANDARAT</p>
-          <div className="user-icon">
-            <a href="{() => false}">  
-              <i className="fas fa-user"></i>
-            </a>
-          </div>
+        <div className="user-icon">
+          <a href="{() => false}">
+            <i className="fas fa-user"></i>
+          </a>
+        </div>
       </div>
       <div className="grid-container">
-        {COLORS.map((COLOR,idx)=> {
-          // 정중앙(센터 of 센터)부분의 경우 : 컬러값 배열로 다 넘기기
-          if(idx === 4) {
-            return (<Box color={COLORS} key={idx}/>)
+        {positions.map((position, index) => {
+          if (index < 4) {
+            return (
+              <Box
+                key={position}
+                centerData={mainTopicData[position]}
+                isActive={mainTopicData[position] !== ""}
+                color={
+                  mainTopicData[position] !== "" ? COLORS[index] : undefined
+                }
+              />
+            );
           }
-          // 3x3의 나머지 8개의 경우 : 컬러 하나만
+
+          if (index === 4) {
+            return (
+              <div className="box-container main-box">
+                {positions.map((position, index) => (
+                  <div
+                    key={index}
+                    className={position}
+                    style={{ backgroundColor: COLORS[index] }}
+                  >
+                    {isEditing === position ? (
+                      <input
+                        type="text"
+                        value={tempData[position] ?? ""}
+                        onChange={(e) =>
+                          setTempData((prev) => ({
+                            ...prev,
+                            [position]: e.target.value,
+                          }))
+                        }
+                        onBlur={() => handleBlur(position)}
+                        className="input-style"
+                      />
+                    ) : (
+                      <i onClick={() => handleEdit(position)}>
+                        {tempData[position] || (
+                          <span style={{ color: "#888" }}>데이터 입력</span>
+                        )}
+                      </i>
+                    )}
+                  </div>
+                ))}
+              </div>
+            );
+          }
+
           return (
-            <Box color={COLOR} key={idx}/>
-          )
+            <Box
+              key={position}
+              centerData={mainTopicData[position]}
+              isActive={mainTopicData[position] !== ""}
+              color={mainTopicData[position] !== "" ? COLORS[index] : undefined}
+            />
+          );
         })}
       </div>
     </>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,78 +1,23 @@
 import "../css/box.style.css";
 
 // 3x3사이즈의 박스 컴포넌트 - 서브페이지에서 사용
-export const Box = ({color}) => {
-  const isColorTrue = Array.isArray(color);
+export const Box = ({ centerData, isActive, color }) => {
   return (
-    <>
-      {
-        isColorTrue
-        ? (
-          <div className="box-container" style={{border: `4px solid ${color[4]}`}}>
-            <div className="one" style={{backgroundColor: color[0]}}>
-              <i>1</i>
-            </div>
-            <div className="two" style={{backgroundColor: color[1]}}>
-              <i>2</i>
-            </div>
-            <div className="three" style={{backgroundColor: color[2]}}>
-              <i>3</i>
-            </div>
-            <div className="four" style={{backgroundColor: color[3]}}>
-              <i>4</i>
-            </div>
-            <div className="sub-center" style={{backgroundColor : color[4]}}>
-              {/* 중앙 부분 컬러가 센터 중 센터색인 경우만 white로 폰트색 지정 / 나머지는 검정*/}
-              {color[4] === "#3A4D8F" ? (<i style={{color: "white"}}>중앙</i>) : (<i>중앙</i>)}
-            </div>
-            <div className="five" style={{backgroundColor: color[5]}}>
-              <i>5</i>
-            </div>
-            <div className="six" style={{backgroundColor: color[6]}}>
-              <i>6</i>
-            </div>
-            <div className="seven" style={{backgroundColor: color[7]}}>
-              <i>7</i>
-            </div>
-            <div className="eight" style={{backgroundColor: color[8]}}>
-              <i>8</i>
-            </div>
-          </div>
-            )
-        : (
-        <div className="box-container" style={{border: `4px solid ${color}`}}>
-          <div className="one">
-            <i>1</i>
-          </div>
-          <div className="two">
-            <i>2</i>
-          </div>
-          <div className="three">
-            <i>3</i>
-          </div>
-          <div className="four">
-            <i>4</i>
-          </div>
-          <div className="sub-center" style={{backgroundColor : color}}>
-            {/* 중앙 부분 컬러가 센터 중 센터색인 경우만 white로 폰트색 지정 / 나머지는 검정
-            {color === "#3A4D8F" ? (<i style={{color: "white"}}>중앙</i>) : (<i>중앙</i>)} */}
-            <i>중앙</i>
-          </div>
-          <div className="five">
-            <i>5</i>
-          </div>
-          <div className="six">
-            <i>6</i>
-          </div>
-          <div className="seven">
-            <i>7</i>
-          </div>
-          <div className="eight">
-            <i>8</i>
-          </div>
-        </div>
-        )
-      }
-    </>
+    <div className="box-container">
+      <div className={`box one ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box two ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box three ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box four ${isActive ? "active" : "inactive"}`}></div>
+      <div
+        className={`box center ${isActive ? "active" : "inactive"}`}
+        style={{ backgroundColor: color}}
+      >
+        {centerData}
+      </div>
+      <div className={`box five ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box six ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box seven ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box eight ${isActive ? "active" : "inactive"}`}></div>
+    </div>
   );
 };
