e92eeba09547276dd1552d0291eaf2a1371678cf
junyeokk <cjh4302@gmail.com>
2023-09-15 19:32:04 +0900

    feat: 가운데 Box 컴포넌트에서 입력하면 그 주변에 있는 컴포넌트로 전이될 수 있도록

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,31 +1,122 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box } from "./components/Box";
 import "./css/main.style.css";
 
-// 서브 페이지 화면
 export const Main = () => {
-  // const [colors, setColors] = useState([]);
+  const [mainTopicData, setMainTopicData] = useState({
+    one: "",
+    two: "",
+    three: "",
+    four: "",
+    five: "",
+    six: "",
+    seven: "",
+    eight: "",
+  });
+  const [isEditing, setIsEditing] = useState(null);
+  const [tempData, setTempData] = useState({});
+
+  const handleDataChange = (position, data) => {
+    setMainTopicData((prev) => ({ ...prev, [position]: data }));
+  };
+
+  const handleEdit = (position) => {
+    setIsEditing(position);
+    setTempData((prev) => ({ ...prev, [position]: mainTopicData[position] }));
+  };
+
+  const handleBlur = (position) => {
+    handleDataChange(position, tempData[position]);
+    setIsEditing(null);
+  };
+
+  const positions = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "center",
+    "five",
+    "six",
+    "seven",
+    "eight",
+  ];
+
+  useEffect(() => {
+    setMainTopicData({
+      one: "",
+      two: "",
+      three: "",
+      four: "",
+      five: "",
+      six: "",
+      seven: "",
+      eight: "",
+    });
+  }, []);
 
   return (
     <>
       <div className="nav">
         <p className="logo">MANDARAT</p>
-          <div className="user-icon">
-            <a href="{() => false}">  
-              <i className="fas fa-user"></i>
-            </a>
-          </div>
+        <div className="user-icon">
+          <a href="{() => false}">
+            <i className="fas fa-user"></i>
+          </a>
+        </div>
       </div>
       <div className="grid-container">
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
+        {positions.map((position, index) => {
+          if (index < 4) {
+            return (
+              <Box
+                key={position}
+                centerData={mainTopicData[position]}
+                isActive={mainTopicData[position] !== ""}
+              />
+            );
+          }
+
+          if (index === 4) {
+            return (
+              <div className="box-container main-box">
+                {positions.map((position, index) => (
+                  <div key={index} className={position}>
+                    {isEditing === position ? (
+                      <input
+                        type="text"
+                        value={tempData[position] ?? ""}
+                        onChange={(e) =>
+                          setTempData((prev) => ({
+                            ...prev,
+                            [position]: e.target.value,
+                          }))
+                        }
+                        onBlur={() => handleBlur(position)}
+                        className="input-style"
+                        placeholder=""
+                      />
+                    ) : (
+                      <i onClick={() => handleEdit(position)}>
+                        {tempData[position] || (
+                          <span style={{ color: "#888" }}>데이터 입력</span>
+                        )}
+                      </i>
+                    )}
+                  </div>
+                ))}
+              </div>
+            );
+          }
+
+          return (
+            <Box
+              key={position}
+              centerData={mainTopicData[position]}
+              isActive={mainTopicData[position] !== ""}
+            />
+          );
+        })}
       </div>
     </>
   );
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,36 +1,20 @@
 import "../css/box.style.css";
 
 // 3x3사이즈의 박스 컴포넌트 - 서브페이지에서 사용
-export const Box = () => {
+export const Box = ({ centerData, isActive }) => {
   return (
     <div className="box-container">
-      <div className="one">
-        <i>1</i>
+      <div className={`box one ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box two ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box three ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box four ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box center ${isActive ? "active" : "inactive"}`}>
+        {centerData}
       </div>
-      <div className="two">
-        <i>2</i>
-      </div>
-      <div className="three">
-        <i>3</i>
-      </div>
-      <div className="four">
-        <i>4</i>
-      </div>
-      <div className="center">
-        <i>중앙</i>
-      </div>
-      <div className="five">
-        <i>5</i>
-      </div>
-      <div className="six">
-        <i>6</i>
-      </div>
-      <div className="seven">
-        <i>7</i>
-      </div>
-      <div className="eight">
-        <i>8</i>
-      </div>
+      <div className={`box five ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box six ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box seven ${isActive ? "active" : "inactive"}`}></div>
+      <div className={`box eight ${isActive ? "active" : "inactive"}`}></div>
     </div>
   );
 };
