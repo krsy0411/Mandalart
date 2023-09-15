d35dfcf6e1b8d6522378015049a228fd0f4c65a9
krsy0411 <krsy0411@naver.com>
2023-09-15 15:01:58 +0900

    이시영 그리드 1차 작업

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,24 @@
+// import { useState } from "react";
 import { Box } from "./components/Box";
+import "./css/main.style.css";
 
+// 서브 페이지 화면
 export const Main = () => {
+  // const [colors, setColors] = useState([]);
+
   return (
     <>
-      <Box />
+      <div className="grid-container">
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
     </>
   );
 };
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,33 +1,7 @@
-import { useEffect } from "react";
 import "../css/box.style.css";
 
+// 3x3사이즈의 박스 컴포넌트 - 서브페이지에서 사용
 export const Box = () => {
-  // 처음 마운팅 된 시점
-  useEffect(() => {
-    const centerToggle = document.querySelector(".center");
-    const items = document.querySelectorAll(".box-container div");
-
-    centerToggle.addEventListener("click", () => {
-      if (!centerToggle.classList.contains("main-active")) {
-        centerToggle.classList.add("main-active");
-
-        items[0].style.transform = "translate(6em,0)";
-        items[1].style.transform = "translate(6em,6em)";
-        items[2].style.transform = "translate(0,6em)";
-        items[3].style.transform = "translate(-6em,6em)";
-        items[4].style.transform = "translate(-6em,0)";
-        items[5].style.transform = "translate(-6em,-6em)";
-        items[6].style.transform = "translate(0,-6em)";
-        items[7].style.transform = "translate(6em,-6em)";
-      } else {
-        items.forEach((item) => {
-          item.style.transform = "translate(0,0)";
-        });
-        centerToggle.classList.remove("main-active");
-      }
-    });
-  }, []);
-
   return (
     <div className="box-container">
       <div className="one">
@@ -42,6 +16,9 @@ export const Box = () => {
       <div className="four">
         <i>4</i>
       </div>
+      <div className="center">
+        <i>중앙</i>
+      </div>
       <div className="five">
         <i>5</i>
       </div>
@@ -53,9 +30,6 @@ export const Box = () => {
       </div>
       <div className="eight">
         <i>8</i>
-      </div>
-      <div className="center">
-        <i>C</i>
       </div>
     </div>
   );
