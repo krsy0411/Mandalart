7532448dc8cd56fd4c59143e8523f93db378eacf
JJyen <gdb1632@naver.com>
2023-09-17 00:23:28 +0900

    내 만다라트

--- src/components/HistoryBox.jsx
+++ src/components/HistoryBox.jsx
@@ -1,16 +1,11 @@
 import "../css/historybox.style.css";
-// 
+import { MdDeleteOutline } from "react-icons/md";
+
 export const HistoryBox = () => {
     return (
-        <>
         <div className="historybox-container">
-            <div className="year">20nn년</div>
-            <div className="basic-mandalart">
-                <span className="main">매력적인 사람</span>
-                <span className="main-mark">기본</span>
-            </div>
-            <div className="sub">전액 장학금</div>
+            <span className="historybox-content">멋진사람</span>
+            <span className="delete"><MdDeleteOutline style={{fontSize: "40", marginRight: "20px", }}/></span>
         </div>
-        </>
     )
 }
