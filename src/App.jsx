bcda7787ea0cc4e90fa3759cad300bec4c18c813
JJyen <gdb1632@naver.com>
2023-09-16 21:45:59 +0900

    로그인페이지

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Main } from "./Main";
 import { History } from "./History";
 import { Profile } from "./Profile";
+import { LogIn } from "./LogIn";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Main />} />
       <Route path="/history" element={<History/>}/>
       <Route path="/profile" element={<Profile/>}/>
+      <Route path="/login" element={<LogIn/>}></Route>
     </Routes>
   );
 }
---
+++ src/LogIn.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./css/login.style.css";
+import { Header } from "./components/Header";
+
+export const LogIn = () => {
+    const [email,setEmail]=useState('');
+    const [pw,setPw]=useState("");
+
+    return (
+        <>
+            <style>
+                { `body {
+                background-color: #F6F6F6;
+                    }`}
+            </style>
+            <Header/>
+            <div className="login-container">
+                <div className="text">Log In</div>
+
+                <div className="login-inputbox">
+                    <div className="login-list">이메일 주소</div>
+                    <input className="login-input"
+                            type="text"
+                            onChange={(e)=>setEmail(e.target.value)}
+                            value={email}/>
+                </div>
+                
+                <div className="login-inputbox">
+                    <div className="login-list">비밀번호</div>
+                    <input className="login-input"
+                            type="password" 
+                            onChange={(e)=>setPw(e.target.value)}
+                            value={pw}/>
+                </div>
+
+                <div className="login-btn">
+                    <button>로그인</button>
+                    <div className="kakao">OR</div>
+                    <div className="kakao">카카오톡으로 계속하기</div>
+                    <div className="signup-btn">회원가입</div>
+                </div>
+            </div>
+            <hr/>
+        </>
+    )
+}
