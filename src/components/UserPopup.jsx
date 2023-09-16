4a3f8db6c4ce5c88d819124d10010506b31b46c4
JJyen <gdb1632@naver.com>
2023-09-16 14:08:53 +0900

    css 수정

--- src/components/UserPopup.jsx
+++ src/components/UserPopup.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/userpopup.style.css";
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import PersonIcon from '@mui/icons-material/Person';
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
+
 
 export const UserPopup = (props) => {
+    const navigate=useNavigate();
+
+    const goHome = ()=> {
+        navigate('/');
+        alert("로그아웃 되었습니다.")
+    }
 
     return (
         <>
@@ -14,18 +25,18 @@ export const UserPopup = (props) => {
                 </div>
                 <Link to="/history" className="custom-link">
                     <div className="a">
-                        <img className='history-icon' alt="1"/>
-                        만다라트 히스토리
+                        <AppsOutlinedIcon className="popup-icon"/>
+                        내 만다라트
                     </div>
                 </Link>
                 <Link to="/profile" className="custom-link">
                     <div className="a">
-                        <img className='profile-icon'  alt="2"/>
+                        <PersonIcon className="popup-icon"/>
                         회원정보
                     </div>
                 </Link>
-                <div className="a">
-                    <img className='logout-icon' alt="3"/>
+                <div className="logout" onClick={goHome}>
+                    <PowerSettingsNewOutlinedIcon className="popup-icon"/>
                     로그아웃</div>
             </div>
             )}
